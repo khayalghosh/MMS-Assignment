69f34ec9e4a5f1575ac995a2ff904c6425738b6e
khayalghosh <36408374+khayalghosh@users.noreply.github.com>
2022-09-17 17:30:21 +0530

    Create component-install.sh

---
+++ component-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#install Azure cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+#install Kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+#install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
